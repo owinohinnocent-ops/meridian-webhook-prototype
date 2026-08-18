b3c5082e7b9e1d10fe87ddc1c6ad33afe93ff299
innocent44 <owinohinnocent@gmail.com>
2026-08-18 23:29:08 +0300

    Build working stock update webhook

---
+++ webhook.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class StockUpdate(BaseModel):
+    product_id: str
+    quantity: int
+
+app = FastAPI()
+
+@app.post("/webhook")
+def receive_stock_update(stock: StockUpdate):
+    print(stock)
